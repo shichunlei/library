218abb4413f89891dbe217512bce5bdfd46333d1
shichunlei <1558053958@qq.com>
2016-09-10 08:04:08 +0800

    添加动画

--- utils/src/com/chingtech/utils/AnimationUtil.java
+++ utils/src/com/chingtech/utils/AnimationUtil.java
@@ -1,5 +1,8 @@
 package com.chingtech.utils;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.View;
 import android.view.animation.Animation;
@@ -8,6 +11,7 @@ import android.view.animation.CycleInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.view.animation.Animation.AnimationListener;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * 动画辅助类
@@ -141,5 +145,98 @@ public class AnimationUtil {
 
 		imageview.setVisibility(View.VISIBLE);
 		imageview.startAnimation(mAnimation);
+	}
+
+	/**
+	 * 数字变化动画
+	 * 
+	 * @param textview
+	 * @param start
+	 * @param end
+	 * @param duration
+	 */
+	public void setValueAnim(final TextView textview, int start, int end, int duration) {
+		ValueAnimator valueAnimator = ValueAnimator.ofInt(start, end);// 数字起始-结束
+		valueAnimator.setTarget(textview);
+		valueAnimator.setDuration(duration);// 动画时间(ms)
+		valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+			@Override
+			public void onAnimationUpdate(ValueAnimator animation) {
+				int i = Integer.valueOf(String.valueOf(animation.getAnimatedValue()));
+				textview.setText(String.valueOf(i));
+			}
+		});
+		valueAnimator.start();
+	}
+
+	/**
+	 * 淡入淡出
+	 * 
+	 * @param view
+	 */
+	public void setViewAlpha(View view) {
+		ObjectAnimator animator = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f, 1f);
+		animator.setDuration(5000);
+		animator.start();
+	}
+
+	/**
+	 * 旋转
+	 * 
+	 * @param view
+	 */
+	public void setViewRotation(View view) {
+		ObjectAnimator animator = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f);
+		animator.setDuration(5000);
+		animator.start();
+	}
+
+	/**
+	 * 水平方向平移
+	 * 
+	 * @param view
+	 */
+	public void setViewTranslationX(View view) {
+		float curTranslationX = view.getTranslationX();
+		ObjectAnimator animator = ObjectAnimator.ofFloat(view, "translationX", curTranslationX, -500f,
+				curTranslationX);
+		animator.setDuration(5000);
+		animator.start();
+	}
+
+	/**
+	 * 在Y轴方向上对View进行缩放
+	 * 
+	 * @param view
+	 */
+	public void setViewScaleY(View view) {
+		ObjectAnimator animator = ObjectAnimator.ofFloat(view, "scaleY", 1f, 3f, 1f);
+		animator.setDuration(5000);
+		animator.start();
+	}
+
+	/**
+	 * 组合动画
+	 * 
+	 * 实现组合动画功能主要需要借助AnimatorSet这个类，这个类提供了一个play()方法，如果我们向这个方法中传入一个Animator对象
+	 * (ValueAnimator或ObjectAnimator
+	 * )将会返回一个AnimatorSet.Builder的实例，AnimatorSet.Builder中包括以下四个方法：
+	 * 
+	 * after(Animator anim) 将现有动画插入到传入的动画之后执行
+	 * 
+	 * after(long delay) 将现有动画延迟指定毫秒后执行
+	 * 
+	 * before(Animator anim) 将现有动画插入到传入的动画之前执行
+	 * 
+	 * with(Animator anim) 将现有动画和传入的动画同时执行
+	 */
+	public void sss(View view) {
+		ObjectAnimator moveIn = ObjectAnimator.ofFloat(view, "translationX", -500f, 0f);
+		ObjectAnimator rotate = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f);
+		ObjectAnimator fadeInOut = ObjectAnimator.ofFloat(view, "alpha", 1f, 0f, 1f);
+		AnimatorSet animSet = new AnimatorSet();
+		animSet.play(rotate).with(fadeInOut).after(moveIn);
+		animSet.setDuration(5000);
+		animSet.start();
 	}
 }
